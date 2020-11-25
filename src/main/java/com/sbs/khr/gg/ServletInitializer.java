0c817c46b22f47683bf69cd2f14e51e81e67042c
khr777 <kim5638yw@gmail.com>
2020-11-25 14:38:04 +0900

    JSP엔진, JSTL, SQL로거pom.xml에 추가/HomeController 생성/main.jsp 생성

---
+++ src/main/java/com/sbs/khr/gg/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sbs.khr.gg;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GgApplication.class);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:set var="title" value="메인"/>
+<%@ include file="../../part/head.jspf" %>
+    
+
+<h1>안녕?</h1>
+
+<%@ include file="../../part/foot.jspf" %>
